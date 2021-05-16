417311c25e1efb6ee6a63f7c2a4c2ece76ad6294
amit <amitoosh.mor@gmail.com>
2021-05-16 23:28:20 +0300

    link

--- chat-app/src/PreJoin.js
+++ chat-app/src/PreJoin.js
@@ -1,31 +1,74 @@
 import React, { useRef, useState, useEffect } from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  useLocation,
-  useHistory,
-} from "react-router-dom";
-
+import { useLocation, useHistory } from "react-router-dom";
+import firebase from "firebase";
+import ActiveChat from "./ActiveChat";
+import { useAuthState } from "react-firebase-hooks/auth";
 export default function PreJoin() {
+  const auth = firebase.auth();
+
+  const [user] = useAuthState(auth);
+  const firestore = firebase.firestore();
+  const roomsref = firestore.collection("rooms");
   let location = useLocation();
   let history = useHistory();
-  console.log(location, "LOC");
+  const [Room, setRoom] = useState();
+  const [authorized, setAuthorized] = useState(false);
+  const [popMessage, setPopMessage] = useState();
+  const roomPassword = useRef();
+
   useEffect(() => {
     console.log("inside");
     const query = location.search.slice(1, 5);
-    if (query === "room") {
-      const joinRoom = location.search.slice(6);
-      if (joinRoom) {
-        // history.push("/prejoin");
-      }
+    const joinRoom = location.search.slice(6);
+    if (!(query === "room") || !joinRoom) history.push("/");
+    roomsref
+      .where("room", "==", joinRoom)
+      .get()
+      .then((querySnapshot) => {
+        console.log(querySnapshot.empty);
+        if (querySnapshot.empty) {
+          alert("there is no such room");
+          history.push("/");
+        }
 
-      // roomsref.where("room" , "=" , joinRoom).
-    }
+        querySnapshot.forEach((doc) => {
+          console.log("hereee");
+          const room = doc.data();
+          console.log(room);
+          setRoom(room);
+        });
+      })
+      .catch((e) => {
+        console.log(e);
+      });
   }, [location.search]);
+
+  const JOINRoom = () => {
+    if (roomPassword.current.value !== Room.password) {
+      roomPassword.current.value = "";
+      return setPopMessage("incorect password");
+    }
+    ///ad user to room.users
+    setAuthorized(true);
+  };
 
   return (
     <div>
-      <p>fffffffffffffffff</p>prejoin
+      <div>
+        <button>back to profile</button>
+      </div>
+      {authorized ? (
+        <div id="active-chat">
+          <ActiveChat activeChat={Room.room} user={user} />
+        </div>
+      ) : (
+        <div>
+          <p>please enter passcode</p>
+          <input ref={roomPassword} required></input>
+          <button onClick={JOINRoom}>submit</button>
+          {popMessage}
+        </div>
+      )}
     </div>
   );
 }
